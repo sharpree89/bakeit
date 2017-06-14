f6028188fa0b1cd1531e65adfebed8e383ef278c
Preeya Sharma <sharpree89@gmail.com>
2017-06-14 14:28:15 -0500

    more css changes

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(passport.initialize());
 app.use('/', index);
 app.use('/users', users);
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
